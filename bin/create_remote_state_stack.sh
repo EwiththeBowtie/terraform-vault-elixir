a226e5dfc5c868d5bb58b43debb22c9cd9a43057
E Einowski <eeinowski@liffft.com>
2019-07-30 16:05:29 -0700

    Update create_remote_state_stack.sh

--- bin/create_remote_state_stack.sh
+++ bin/create_remote_state_stack.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-aws cloudformation create-stack --stack-name tf-vault-elixir-remote-state --template-body file://./remote-state.yml --profile default
-
+aws cloudformation create-stack --stack-name tf-vault-elixir-remote-state --template-body file://./cloudformation/remote-state.yml --profile default
