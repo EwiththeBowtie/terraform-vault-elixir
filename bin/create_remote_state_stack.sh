fd6bd17bb893086d4611c397ddc8159cc89910e3
E Einowski <eeinowski@liffft.com>
2019-07-30 17:02:54 -0700

    Add capabilities

--- bin/create_remote_state_stack.sh
+++ bin/create_remote_state_stack.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-aws cloudformation create-stack --stack-name tf-vault-elixir-remote-state --template-body file://./cloudformation/remote-state.yml --profile default
+aws cloudformation create-stack --stack-name tf-vault-elixir-remote-state --template-body file://./cloudformation/remote-state.yml --profile default --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
