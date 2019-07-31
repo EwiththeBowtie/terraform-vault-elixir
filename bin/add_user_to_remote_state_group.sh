26fb4d3bc206b6907315caf3d029978765d12dbe
E Einowski <eeinowski@liffft.com>
2019-07-30 17:26:46 -0700

    Adds feedback if username isn't supplied

--- bin/add_user_to_remote_state_group.sh
+++ bin/add_user_to_remote_state_group.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-set -ev
+set -e
+if [[ $# -ne 1 ]]; then
+  echo "Please supply an aws username"
+  exit 1
+fi
 aws iam add-user-to-group --user-name "$1" --group-name TfRemoteStateGroup
