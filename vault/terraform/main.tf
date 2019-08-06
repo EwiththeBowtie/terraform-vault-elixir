8a8f6c2e7f1779e8c8efaf2cfbf075b58a8dad3f
E Einowski <eeinowski@liffft.com>
2019-08-06 14:45:13 -0700

    Adds kms key attacted to vault instance profile

--- vault/terraform/main.tf
+++ vault/terraform/main.tf
@@ -206,3 +206,32 @@ module "vault_aws" {
   tags             = "${var.vault_tags}"
   tags_list        = "${var.vault_tags_list}"
 }
+
+resource "random_pet" "env" {
+  length    = 2
+  separator = "_"
+}
+
+resource "aws_kms_key" "vault" {
+  description             = "Vault unseal key"
+  deletion_window_in_days = 10
+
+  tags = {
+    Name = "vault-kms-unseal-${random_pet.env.id}"
+  }
+}
+
+data "aws_iam_policy_document" "vault-kms-unseal" {
+  statement {
+    sid       = "VaultKMSUnseal"
+    effect    = "Allow"
+    resources = ["${aws_kms_key.vault.arn}"]
+
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:DescribeKey",
+    ]
+  }
+}
+						
