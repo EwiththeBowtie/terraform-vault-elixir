fb0fadfb10582d5add6f0e365c773aa21d92daa0
E Einowski <eeinowski@gmail.com>
2019-08-20 15:21:24 -0700

    Delete add_current_user_to_vault.sh

--- bin/add_current_user_to_vault.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -ex
-
-CURRENT_USER_ARN=$(aws sts get-caller-identity | jq --raw-output .Arn)
-
-vault write -tls-skip-verify auth/aws/role/dev-role-iam auth_type=iam bound_iam_principal_arn=$CURRENT_USER_ARN policies=dev max_ttl=24h 
